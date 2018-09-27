81a5545d5d474643ad549e0cbd7cc4c80d89fe0e
Federico Aloi <federico.aloi@gmail.com>
2018-09-27 18:54:09 -0300

    Implementado momento de crisis

--- app/estado.js
+++ app/estado.js
@@ -3,6 +3,7 @@ module.exports =
 class Estado {
   constructor() {
     this.presupuestoEjecutado = 0;
+    this.deudaFmi = 0;
     this.dineroDisponible = 100000;
   }
 
@@ -12,6 +13,10 @@ class Estado {
   }
 
   ejecutarPartida(ministerio, partida) {
+    if (this.momentoDeCrisis) {
+      this.deudaFmi += partida / 3;
+      partida *= 2 / 3;
+    }
     ministerio.recibirPartida(partida);
     this.presupuestoEjecutado += partida;
     this.dineroDisponible -= partida;
